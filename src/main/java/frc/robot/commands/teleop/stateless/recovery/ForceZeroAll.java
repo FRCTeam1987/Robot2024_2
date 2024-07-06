3a572f9d253720df25f2eba7afa710f6c631b243
Lithiuman <lithiumxn@gmail.com>
2024-07-06 14:21:31 -0500

    Fix Build Issue

--- src/main/java/frc/robot/commands/teleop/stateless/recovery/ForceZeroAll.java
+++ src/main/java/frc/robot/commands/teleop/stateless/recovery/ForceZeroAll.java
@@ -5,43 +5,26 @@
 package frc.robot.commands.teleop.stateless.recovery;
 
 import static frc.robot.RobotContainer.ELEVATOR;
-import static frc.robot.RobotContainer.INTAKE;
-import static frc.robot.RobotContainer.SHOOTER;
 import static frc.robot.RobotContainer.WRIST;
 import static frc.robot.RobotContainer.setRobotState;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.Constants;
 import frc.robot.commands.teleop.logic.RobotState;
-import frc.robot.subsystems.Elevator;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
-import frc.robot.subsystems.Wrist;
 import frc.robot.util.InstCmd;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class ReverseIntake extends SequentialCommandGroup {
+public class ForceZeroAll extends SequentialCommandGroup {
     /** Creates a new ReverseIntake. */
-    public ReverseIntake() {
+    public ForceZeroAll() {
 
         addCommands(
                 new InstCmd(() -> setRobotState(RobotState.RECOVERY)),
                 new InstCmd(
                         () -> {
-                            SHOOTER.setFeederVoltage(-Constants.Shooter.FEEDER_FEEDFWD_VOLTS);
-                            INTAKE.setVolts(-Constants.Intake.INTAKE_COLLECT_VOLTS);
-                            WRIST.setDegrees(21);
-                            ELEVATOR.goHome();
-                        }),
-                new WaitCommand(3.0),
-                new InstCmd(
-                        () -> {
-                            SHOOTER.stopFeeder();
-                            INTAKE.stopBoth();
+                            WRIST.setZero();
+                            ELEVATOR.setZero();
                         }),
                 new InstCmd(() -> setRobotState(RobotState.DEFAULT)));
     }
